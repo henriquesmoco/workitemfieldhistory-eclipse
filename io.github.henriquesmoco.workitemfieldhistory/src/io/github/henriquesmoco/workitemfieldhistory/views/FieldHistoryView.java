7ab511d471d668cc36c277a922c684628f122d94
Henrique Smoco <henriquesmoco@outlook.com>
2014-08-21 02:26:47 -0300

    Adicionado onClick no botão Show Revisions

--- io.github.henriquesmoco.workitemfieldhistory/src/io/github/henriquesmoco/workitemfieldhistory/views/FieldHistoryView.java
+++ io.github.henriquesmoco.workitemfieldhistory/src/io/github/henriquesmoco/workitemfieldhistory/views/FieldHistoryView.java
@@ -4,8 +4,13 @@ import java.util.Optional;
 import java.util.regex.Pattern;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.part.ViewPart;
@@ -15,10 +20,10 @@ public class FieldHistoryView extends ViewPart {
 	private static final Pattern ONLY_DIGITS = Pattern.compile("\\d+");
 	private Text txtWorkItemId;
 	private Button btnShowRevisions;
+	private Group grpWorkItem;
 	
 	@Override
-	public void createPartControl(Composite parent) {
-		Composite composite = new Composite(parent, SWT.NONE);
+	public void createPartControl(Composite composite) {
 		Label lblId = new Label(composite, SWT.LEFT);
 		lblId.setText("Work Item ID:");
 		txtWorkItemId = new Text(composite, SWT.SINGLE | SWT.BORDER);
@@ -31,7 +36,25 @@ public class FieldHistoryView extends ViewPart {
 		
 		btnShowRevisions = new Button(composite, SWT.PUSH);
 		btnShowRevisions.setText("Show Revision(s)");
+		btnShowRevisions.addListener(SWT.Selection, evt -> {
+			String msg = "[Work Item not found]";
+			grpWorkItem.setText(msg);
+		});
+		
+		grpWorkItem = new Group(composite, SWT.NONE);
+		grpWorkItem.setText("");
+		
+		
 	}
+	
+	@Override
+	public void setFocus() {
+		txtWorkItemId.setFocus();		
+	}
+	
+	//----------------------------
+	// Methods used for automation
+	//----------------------------
 	
 	public void setWorkItemId(String text) {
 		txtWorkItemId.setText(text);
@@ -41,14 +64,16 @@ public class FieldHistoryView extends ViewPart {
 		return txtWorkItemId.getText();
 	}
 
-	@Override
-	public void setFocus() {
-		// TODO Auto-generated method stub
-		
-	}
-
 	public boolean isShowRevisionsEnabled() {
 		return btnShowRevisions.isEnabled();
+	}
+
+	public void showRevisionsClick() {
+		btnShowRevisions.notifyListeners(SWT.Selection, null);
+	}
+
+	public String getWorkItemTitle() {
+		return grpWorkItem.getText();
 	}
 
 }
