45a89b40e05abc5f359b96e126cc1443eb8c2ace
Henrique Smoco <henriquesmoco@outlook.com>
2014-08-21 01:25:38 -0300

    Adicionado validação do campo Work Item ID

--- io.github.henriquesmoco.workitemfieldhistory/src/io/github/henriquesmoco/workitemfieldhistory/views/FieldHistoryView.java
+++ io.github.henriquesmoco.workitemfieldhistory/src/io/github/henriquesmoco/workitemfieldhistory/views/FieldHistoryView.java
@@ -1,6 +1,10 @@
 package io.github.henriquesmoco.workitemfieldhistory.views;
 
+import java.util.Optional;
+import java.util.regex.Pattern;
+
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
@@ -8,13 +12,25 @@ import org.eclipse.ui.part.ViewPart;
 
 public class FieldHistoryView extends ViewPart {
 	public static final String ID = "io.github.henriquesmoco.workitemfieldhistory.views.FieldHistoryView";
+	private static final Pattern ONLY_DIGITS = Pattern.compile("\\d+");
 	private Text txtWorkItemId;
+	private Button btnShowRevisions;
 	
 	@Override
-	public void createPartControl(Composite composite) {
+	public void createPartControl(Composite parent) {
+		Composite composite = new Composite(parent, SWT.NONE);
 		Label lblId = new Label(composite, SWT.LEFT);
-		lblId.setText("WorkItem ID:");
-		txtWorkItemId = new Text(composite, SWT.SINGLE | SWT.BORDER);		
+		lblId.setText("Work Item ID:");
+		txtWorkItemId = new Text(composite, SWT.SINGLE | SWT.BORDER);
+		txtWorkItemId.addModifyListener(evt -> {
+			String eventText = ((Text)evt.getSource()).getText();
+			Optional<String> str = Optional.ofNullable(eventText);
+			boolean hasOnlyDigits = ONLY_DIGITS.matcher(str.orElse("")).matches();
+			btnShowRevisions.setEnabled(hasOnlyDigits);
+		});
+		
+		btnShowRevisions = new Button(composite, SWT.PUSH);
+		btnShowRevisions.setText("Show Revision(s)");
 	}
 	
 	public void setWorkItemId(String text) {
@@ -32,7 +48,7 @@ public class FieldHistoryView extends ViewPart {
 	}
 
 	public boolean isShowRevisionsEnabled() {
-		return false;
+		return btnShowRevisions.isEnabled();
 	}
 
 }
